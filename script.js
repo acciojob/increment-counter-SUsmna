61d08c3e235fee841b00cc0a3228de4b807b7941
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-06-04 21:02:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 let count = 0;
 function increment(){
-	alert("un-incremented value:"+ count)
+	alert("un-incremented value."+ count)
 	count++;
 	document.getElementById('counter').innerText = count;
 }
